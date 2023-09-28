553b0d433dba4fd2fbe305f619b227f90fd8be0d
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2023-09-28 11:47:52 +0200

    added Titles to images

--- carouselData.ts
+++ carouselData.ts
@@ -3,25 +3,25 @@ import { CarouselImage } from "./components/SnapCarousel";
 export const fantasyImages: CarouselImage[] = [
   {
     id: 1,
-    title: "Item 1",
+    title: "Mad Hatter",
     text: "This is space for text",
     image: require("./images/Hatter.png"),
   },
   {
     id: 2,
-    title: "Item 2",
+    title: "Tea party",
     text: "This is space for text",
     image: require("./images/Hatter-tea-party.png"),
   },
   {
     id: 3,
-    title: "Item 3",
+    title: "Cheshire",
     text: "This is space for text",
     image: require("./images/Cheshire.png"),
   },
   {
     id: 4,
-    title: "Item 4",
+    title: "Alice",
     text: "This is space for text",
     image: require("./images/Alice.png"),
   },
@@ -30,37 +30,37 @@ export const fantasyImages: CarouselImage[] = [
 export const horrorImages: CarouselImage[] = [
   {
     id: 6,
-    title: "Item 6",
+    title: "Lady white",
     text: "This is space for text",
     image: require("./images/White-hair-faired-lady.png"),
   },
   {
     id: 7,
-    title: "Item 7",
+    title: "Throne lady",
     text: "This is space for text",
     image: require("./images/Lady-on-a-throne.png"),
   },
   {
     id: 8,
-    title: "Item 8",
+    title: "Diabolos",
     text: "This is space for text",
     image: require("./images/Diabolos.png"),
   },
   {
     id: 9,
-    title: "Item 9",
+    title: "Daemon",
     text: "This is space for text",
     image: require("./images/Daemon.png"),
   },
   {
     id: 10,
-    title: "Item 10",
+    title: "Veined lady",
     text: "This is space for text",
     image: require("./images/Woman-with-veins-purple-shaded-eyes.png"),
   },
   {
     id: 11,
-    title: "Item 11",
+    title: "Bat-winged lady",
     text: "This is space for text",
     image: require("./images/Bat-winged-woman.png"),
   },
@@ -68,19 +68,19 @@ export const horrorImages: CarouselImage[] = [
 export const sciFiImages: CarouselImage[] = [
   {
     id: 5,
-    title: "Item 5",
+    title: "Diffusion",
     text: "This is space for text",
     image: require("./images/Diffusion.png"),
   },
   {
     id: 12,
-    title: "Item 12",
+    title: "Edgerunner",
     text: "This is space for text",
     image: require("./images/EdgeRunner.png"),
   },
   {
     id: 13,
-    title: "Item 13",
+    title: "Night City",
     text: "This is space for text",
     image: require("./images/NightCity.png"),
   },
