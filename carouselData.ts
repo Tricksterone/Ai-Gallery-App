0a8c12a1e922c4bf94c620f14791079f566c2064
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2023-09-28 10:02:16 +0200

    moved imageData to carouselData.ts

---
+++ carouselData.ts
@@ -0,0 +1,75 @@
+import { CarouselImage } from "./components/SnapCarousel";
+
+export const fantasyImages: CarouselImage[] = [
+  {
+    id: 1,
+    title: "Item 1",
+    text: "This is space for text",
+    image: require("./images/Hatter.png"),
+  },
+  {
+    id: 2,
+    title: "Item 2",
+    text: "This is space for text",
+    image: require("./images/Hatter-tea-party.png"),
+  },
+  {
+    id: 3,
+    title: "Item 3",
+    text: "This is space for text",
+    image: require("./images/Cheshire.png"),
+  },
+  {
+    id: 4,
+    title: "Item 4",
+    text: "This is space for text",
+    image: require("./images/Alice.png"),
+  },
+];
+
+export const horrorImages: CarouselImage[] = [
+  {
+    id: 6,
+    title: "Item 6",
+    text: "This is space for text",
+    image: require("./images/White-hair-faired-lady.png"),
+  },
+  {
+    id: 7,
+    title: "Item 7",
+    text: "This is space for text",
+    image: require("./images/Lady-on-a-throne.png"),
+  },
+  {
+    id: 8,
+    title: "Item 8",
+    text: "This is space for text",
+    image: require("./images/Diabolos.png"),
+  },
+  {
+    id: 9,
+    title: "Item 9",
+    text: "This is space for text",
+    image: require("./images/Daemon.png"),
+  },
+  {
+    id: 10,
+    title: "Item 10",
+    text: "This is space for text",
+    image: require("./images/Woman-with-veins-purple-shaded-eyes.png"),
+  },
+  {
+    id: 11,
+    title: "Item 11",
+    text: "This is space for text",
+    image: require("./images/Bat-winged-woman.png"),
+  },
+];
+export const sciFiImages: CarouselImage[] = [
+  {
+    id: 5,
+    title: "Item 5",
+    text: "This is space for text",
+    image: require("./images/Diffusion.png"),
+  },
+];
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,102 +1,25 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Button, ScrollView } from "react-native";
+import { ScrollView } from "react-native";
 import { RootStackParamList } from "../App";
-import SnapCarousel, {
-  CarouselData,
-  CarouselImage,
-} from "../components/SnapCarousel";
+import { fantasyImages, horrorImages, sciFiImages } from "../carouselData";
+import SnapCarousel, { CarouselData } from "../components/SnapCarousel";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 export default function HomeScreen({ navigation }: Props) {
   return (
     <ScrollView>
-      <Button
+      {/* <Button
         title="Go to Welcome Page"
         onPress={() => navigation.navigate("Welcome")}
-      />
+      /> */}
       {carouselDataList.map((carouselData) => (
         <SnapCarousel title={carouselData.title} images={carouselData.images} />
       ))}
     </ScrollView>
   );
 }
-
-const fantasyImages: CarouselImage[] = [
-  {
-    id: 1,
-    title: "Item 1",
-    text: "This is space for text",
-    image: require("../images/Hatter.png"),
-  },
-  {
-    id: 2,
-    title: "Item 2",
-    text: "This is space for text",
-    image: require("../images/Hatter-tea-party.png"),
-  },
-  {
-    id: 3,
-    title: "Item 3",
-    text: "This is space for text",
-    image: require("../images/Cheshire.png"),
-  },
-  {
-    id: 4,
-    title: "Item 4",
-    text: "This is space for text",
-    image: require("../images/Alice.png"),
-  },
-];
-
-const horrorImages: CarouselImage[] = [
-  {
-    id: 5,
-    title: "Item 5",
-    text: "This is space for text",
-    image: require("../images/Diffusion.png"),
-  },
-  {
-    id: 6,
-    title: "Item 6",
-    text: "This is space for text",
-    image: require("../images/White-hair-faired-lady.png"),
-  },
-  {
-    id: 7,
-    title: "Item 7",
-    text: "This is space for text",
-    image: require("../images/Lady-on-a-throne.png"),
-  },
-  {
-    id: 8,
-    title: "Item 8",
-    text: "This is space for text",
-    image: require("../images/Diabolos.png"),
-  },
-];
-
-const sciFiImages: CarouselImage[] = [
-  {
-    id: 9,
-    title: "Item 9",
-    text: "This is space for text",
-    image: require("../images/Daemon.png"),
-  },
-  {
-    id: 10,
-    title: "Item 10",
-    text: "This is space for text",
-    image: require("../images/Woman-with-veins-purple-shaded-eyes.png"),
-  },
-  {
-    id: 11,
-    title: "Item 11",
-    text: "This is space for text",
-    image: require("../images/Bat-winged-woman.png"),
-  },
-];
 
 const carouselDataList: CarouselData[] = [
   {
