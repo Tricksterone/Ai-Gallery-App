fb2651e2329e286b521cdc89ff2444d0e8f6d6a5
Tricksterone <113665954+Tricksterone@users.noreply.github.com>
2023-10-03 13:05:09 +0200

    added text to images data

--- carouselData.ts
+++ carouselData.ts
@@ -4,25 +4,25 @@ export const fantasyImages: CarouselImage[] = [
   {
     id: 1,
     title: "Mad Hatter",
-    text: "This is space for text",
+    text: "the mad hatter, glow on his face, golden eye with slight glow, staring directly at you, whimsical orange hued hair, big top hat, card in top hat, purple color on eye and lips, blue hue on face with strong blue background,  (upper torso), full body in frame, centered body, (male:1.2), realistic, ((intricate details)), (pale gothic), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus ",
     image: require("./images/Hatter.png"),
   },
   {
     id: 2,
     title: "Tea party",
-    text: "This is space for text",
+    text: "female guests only In the heart of Wonderland, a hidden grove concealed from the rest of the world, there existed a tea party like no other. The table was adorned with a kaleidoscope of flowers and surrounded by curious, talking guests, each with their own peculiar habits and whims. The table was set with mismatched cups and saucers of different sizes As the tea was poured from teapots that never emptied, conversations drifted into the realms of nonsense and imagination. of their travels through time, never quite sure which century they were in. , which were filled with fantastical adventures involving teacups and sugar cubes. always in a hurry, surprisingly found a moment of respite at this enchanting gathering. they regaled the others with tales of their many encounters with the eccentric residents of Wonderland, all while occasionally checking the pocket watch, which seemed to have a mind of its own. the tea party guests found themselves delving into philosophical discussions about the nature of Wonderland and the meaning of their existence in this surreal world.",
     image: require("./images/Hatter-tea-party.png"),
   },
   {
     id: 3,
     title: "Cheshire",
-    text: "This is space for text",
+    text: "the cheshire cat with big glowing eyes and a big smirk on his face in the dark forest watching you, gothich themed, strong contrasted colors, hues of shades, realistic, even eyes, perfect eyes, ",
     image: require("./images/Cheshire.png"),
   },
   {
     id: 4,
     title: "Alice",
-    text: "This is space for text",
+    text: "blonde (golden) hair, big eyes, dark blue hue on eyes ,running away from creatures, cards in background, gothic themed castle, disney styled castle, Alice in wonderland running from nightmarish creatures from her dreams, in a world she doesnt know, in the castle of cards",
     image: require("./images/Alice.png"),
   },
 ];
@@ -31,37 +31,37 @@ export const horrorImages: CarouselImage[] = [
   {
     id: 6,
     title: "Lady white",
-    text: "This is space for text",
+    text: "girl with a pretty face, white hair, purple eyes, with a funny expression on her face, Hellwalker, incombing death, hell, black bloody veins growing and intertwining out of the darkness, oozing thick yellow blood, veins growing and pumping blood, (female body:1.3), vascular networks growing, connecting, explanding, red veins everywhere, zdzislaw beksinski, (sharp colors:1.3), (rainbow skin:1.1), (Infrared:1.2), ultra detailed, intricate, oil on canvas, ((dry brush, ultra sharp)), (surrealism:1.4), (disturbing:1.5), beksinski style painting, satanic symbols, (full torso), full body in frame, centered body, (male:1.2), realistic, ((intricate details)), (pale gothic evil king), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus",
     image: require("./images/White-hair-faired-lady.png"),
   },
   {
     id: 7,
     title: "Throne lady",
-    text: "This is space for text",
+    text: "girl with a pretty face, golden hair, purple eyes,, with a calm expression on her face, Hellwalker, incombing death, hell, black bloody veins growing and intertwining out of the darkness, oozing thick yellow blood, veins growing and pumping blood, (female body:1.3), vascular networks growing, connecting, expanding, red veins everywhere, zdzislaw beksinski, (sharp colors:1.3), (rainbow skin:1.1), (Infrared:1.2), ultra detailed, intricate, oil on canvas, ((dry brush, ultra sharp)), (surrealism:1.4), (disturbing:1.5), beksinski style painting, satanic symbols, (full torso), full body in frame, centered body, (male:1.2), realistic, ((intricate details)), (pale gothic evil king), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus, sitting on a throne",
     image: require("./images/Lady-on-a-throne.png"),
   },
   {
     id: 8,
     title: "Diabolos",
-    text: "This is space for text",
+    text: "white hair, purple eyes, with a serious expression on his face, Hellwalker, incombing death, hell, black bloody veins growing and intertwining out of the darkness, oozing thick yellow blood, veins growing and pumping blood, vascular networks growing, connecting, explanding, red veins everywhere, zdzislaw beksinski, (sharp colors:1.3), (rainbow skin:1.1), (Infrared:1.2), ultra detailed, intricate, oil on canvas, ((dry brush, ultra sharp)), (surrealism:1.4), (disturbing:1.5), beksinski style painting, satanic symbols, (full torso), full body in frame, centered body, (male:1.2), realistic, ((intricate details)), (pale gothic evil king), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus",
     image: require("./images/Diabolos.png"),
   },
   {
     id: 9,
     title: "Daemon",
-    text: "This is space for text",
+    text: "white hair, purple eyes, with a serious expression on his face, Hellwalker, incombing death, hell, black bloody veins growing and intertwining out of the darkness, oozing thick yellow blood, veins growing and pumping blood, vascular networks growing, connecting, explanding, red veins everywhere, zdzislaw beksinski, (sharp colors:1.3), (rainbow skin:1.1), (Infrared:1.2), ultra detailed, intricate, oil on canvas, ((dry brush, ultra sharp)), (surrealism:1.4), (disturbing:1.5), beksinski style painting, satanic symbols, (full torso), full body in frame, centered body, (male:1.2), realistic, ((intricate details)), (pale gothic evil king), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus",
     image: require("./images/Daemon.png"),
   },
   {
     id: 10,
     title: "Veined lady",
-    text: "This is space for text",
+    text: "crown, crown made of her veins reaching out afar ,girl with a pretty face, white hair, purple eyes, with a funny expression on her face, Hellwalker, incombing death, hell, black bloody veins growing and intertwining out of the darkness, oozing thick yellow blood, veins growing and pumping blood, (female body:1.3), vascular networks growing, connecting, explanding, red veins everywhere, zdzislaw beksinski, (sharp colors:1.3), (rainbow skin:1.1), (Infrared:1.2), ultra detailed, intricate, oil on canvas, ((dry brush, ultra sharp)), (surrealism:1.4), (disturbing:1.5), beksinski style painting, satanic symbols, (full torso), full body in frame, centered body, (male:1.2), realistic, ((intricate details)), (pale gothic evil king), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus",
     image: require("./images/Woman-with-veins-purple-shaded-eyes.png"),
   },
   {
     id: 11,
     title: "Bat-winged lady",
-    text: "This is space for text",
+    text: "crown with wings, massive batwings in background,girl with a pretty face, white hair, purple eyes, with a funny expression on her face, Hellwalker, incombing death, hell, black bloody veins growing and intertwining out of the darkness, oozing thick yellow blood, veins growing and pumping blood, (female body:1.3), vascular networks growing, connecting, explanding, red veins everywhere, zdzislaw beksinski, (sharp colors:1.3), (rainbow skin:1.1), (Infrared:1.2), ultra detailed, intricate, oil on canvas, ((dry brush, ultra sharp)), (surrealism:1.4), (disturbing:1.5), beksinski style painting, satanic symbols, (full torso), full body in frame, centered body, realistic, ((intricate details)), (pale gothic queen), dynamic pose, perfect face, (realistic eyes), perfect eyes, ((dark gothic background)), sharp focus",
     image: require("./images/Bat-winged-woman.png"),
   },
 ];
@@ -75,13 +75,13 @@ export const sciFiImages: CarouselImage[] = [
   {
     id: 12,
     title: "Edgerunner",
-    text: "This is space for text",
+    text: "In the neon-drenched streets of Night City, where technology and chaos collide, a group of edgerunners known for their daring exploits and renegade spirit operates in the shadows. These edgerunners, each with their own unique cyberware enhancements and troubled pasts, are drawn together by a shared desire for survival and freedom in a city ruled by corporations and teeming with danger. Among them is a charismatic netrunner who can breach even the most secure systems with a flick of their wrist, a cyber-samurai with blades of steel and a code of honor, a streetwise fixer who always knows where to find the best black-market deals, and a rogue AI with a mysterious agenda. As Night City's underbelly churns with intrigue and conspiracy, the edgerunners find themselves entangled in a web of corporate espionage, political power plays, and the relentless pursuit of forbidden knowledge. They navigate the neon-lit labyrinth of back-alley deals, cyber-enhanced mercenaries, and virtual realities that blur the line between the real and the digital.",
     image: require("./images/EdgeRunner.png"),
   },
   {
     id: 13,
     title: "Night City",
-    text: "This is space for text",
+    text: "In the neon-drenched streets of Night City, where technology and chaos collide, a group of edgerunners known for their daring exploits and renegade spirit operates in the shadows. These edgerunners, each with their own unique cyberware enhancements and troubled pasts, are drawn together by a shared desire for survival and freedom in a city ruled by corporations and teeming with danger. Among them is a charismatic netrunner who can breach even the most secure systems with a flick of their wrist, a cyber-samurai with blades of steel and a code of honor, a streetwise fixer who always knows where to find the best black-market deals, and a rogue AI with a mysterious agenda. As Night City's underbelly churns with intrigue and conspiracy, the edgerunners find themselves entangled in a web of corporate espionage, political power plays, and the relentless pursuit of forbidden knowledge. They navigate the neon-lit labyrinth of back-alley deals, cyber-enhanced mercenaries, and virtual realities that blur the line between the real and the digital. Together, they must uncover the truth about a city that never sleeps, a city that hungers for their secrets and thrives on their desperation. But in a world where betrayal is as common as implants, who can they trust? And what sacrifices are they willing to make to survive in a city that preys on the dreams of the desperate and the ambitions of the daring?",
     image: require("./images/NightCity.png"),
   },
 ];
